a35efe1fd0613a0bb20628c8c87a0d5f84ae9713
DeeaKr <korodiandreea@yahoo.com>
2020-10-14 15:41:39 +0300

    some changes

--- lab2/src/main/java/Main.java
+++ lab2/src/main/java/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println(hashSymbolTable.printTable());
         System.out.println( hashSymbolTable.findBySymbol("braved").toString());
         System.out.println( hashSymbolTable.findBySymbol("adverb").toString());
+        System.out.println(hashSymbolTable.findBySymbol("kk").toString());
 
 
     }
